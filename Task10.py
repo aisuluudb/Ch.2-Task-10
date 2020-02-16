040022889b1322751af79e479d790914dff39bb5
Aisuluu DB <dzhaisuluu@gmail.com>
2020-02-16 20:58:37 +0600

    Add files via upload

---
+++ Task10.py
@@ -0,0 +1,10 @@
+my_string = "Aisuluu"
+print (my_string[3])
+print (my_string[2:7])
+print (my_string[:6])
+print (my_string[:5])
+print (my_string[2],my_string[4],my_string[6])
+print (my_string[1], my_string[3], my_string[5])
+print (my_string[::-1]) #reverse order
+print (my_string [-2], my_string[-4], my_string[-6])
+print (len(my_string))
